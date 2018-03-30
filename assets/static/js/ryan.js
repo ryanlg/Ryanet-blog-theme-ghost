e7a41e1f8e44e419be640fb0e69a54dac93fd663
Ryan Liang <xmomowan@gmail.com>
2018-03-30 14:52:10 -0500

    add reset.scss and fix accordingly

--- assets/static/js/ryan.js
+++ assets/static/js/ryan.js
@@ -1,12 +1,12 @@
-window.onload = function(){
-  $("#sidemenu-profile-image").mouseenter(function(){$('#sidemenu-container').show(200);});
-  $("#sidemenu-container").mouseleave(function(){$('#sidemenu-container').fadeOut(200);});
+// window.onload = function(){
+//   $("#sidemenu-profile-image").mouseenter(function(){$('#sidemenu-container').show(200);});
+//   $("#sidemenu-container").mouseleave(function(){$('#sidemenu-container').fadeOut(200);});
 
-  var imgs = $('img');
-  $.each(imgs, function(i, value){
-    var v = $(value);
-    if(v.parent().parent().attr('class') == 'post-content'){
-      v.parent().css("text-align", "center");
-    }
-  })
-}
+//   var imgs = $('img');
+//   $.each(imgs, function(i, value){
+//     var v = $(value);
+//     if(v.parent().parent().attr('class') == 'post-content'){
+//       v.parent().css("text-align", "center");
+//     }
+//   })
+// }
